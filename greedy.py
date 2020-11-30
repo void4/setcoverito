1f0af538b7f07c824585abcd84b51bc0c709abd9
void4 <arndtschnabel@freenet.de>
2020-11-30 16:59:06 +0100

    Add completeness tests

--- greedy.py
+++ greedy.py
@@ -8,14 +8,22 @@ print(ingredients)
 things = {}
 
 for row in list(reader):
-	things[row[0]] = {i for i,c in enumerate(row[1:]) if c!=""}	
-	
+	things[row[0]] = {i for i,c in enumerate(row[1:]) if c!=""}
+
 print(len(ingredients))
 mcover = set()
 cover = set()
 while len(cover) != len(ingredients):
-	
+
 	mx = max(things.items(), key=lambda thing:thing[1]-cover)
 	cover = cover.union(mx[1])
 	mcover.add(mx[0])
 	print(len(cover), len(mcover), cover, mcover)
+
+test = [False for i in range(len(ingredients))]
+for ingredient in mcover:
+	for x in things[ingredient]:
+		test[x] = True
+
+#print(test)
+print(all(test))
--- heuristic.py
+++ heuristic.py
@@ -7,21 +7,22 @@ print(ingredients)
 
 matrix = []
 
-
-
 for row in list(reader):
-	matrix.append([True if c!="" else False for i,c in enumerate(row[1:])])
+	matrix.append([c!="" for i,c in enumerate(row[1:])])
 	
 print(matrix)
 	
 matrix = np.array(matrix).T
 cost = np.array([1 for i in range(len(matrix[0]))])
-	
+
 from setcover import SetCover
-g = SetCover(matrix, cost)
+g = SetCover(matrix, cost)#, maxiters=100, subg_nsteps=20, subg_maxiters=200)
 solution, time_used = g.SolveSCP()
 print(solution)
 print(g.s)
+
+#solution = g.greedy()
+#print(solution)
 
 test = [False for i in range(len(matrix[0]))]
 
@@ -35,3 +36,5 @@ for i,s in enumerate(g.s):
 
 print(test)
 print(all(test))
+
+print()
