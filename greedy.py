36ac2bd3660b03d8c10e7e0d3d16e329d44e1b20
void4 <arndtschnabel@freenet.de>
2020-12-01 02:23:33 +0100

    Transpose algorithm, get dishes for ingredients, not vice versa

--- greedy.py
+++ greedy.py
@@ -2,27 +2,41 @@ import csv
 
 reader = csv.reader(open("MenuIngredients.csv"))
 
-ingredients = next(reader)[2:]
-print(ingredients)
+dishes = next(reader)[2:]
+ingredients = []
+
+print("dishes", dishes, len(dishes))
+
+rows = list(reader)
+columns = []
+
+for row in rows:
+	ingredients.append(row[0])
+
+for ci in range(len(rows[0])-2):
+	columns.append([])
+	for row in rows:
+		columns[-1].append(row[2+ci])
+#print(ingredients)
 
 things = {}
+for ci, column in enumerate(columns):
+	things[dishes[ci]] = {i for i,c in enumerate(column) if c!=""}
 
-for row in list(reader):
-	things[row[0]] = {i for i,c in enumerate(row[2:]) if c!=""}
+print(things)
 
-print(len(ingredients))
 mcover = set()
 cover = set()
 while len(cover) != len(ingredients):
-
 	mx = max(things.items(), key=lambda thing:thing[1]-cover)
 	cover = cover.union(mx[1])
 	mcover.add(mx[0])
-	print(len(cover), len(mcover), cover, mcover)
+	print(len(cover), len(mcover))
+	print(cover, mcover)
 
 test = [False for i in range(len(ingredients))]
-for ingredient in mcover:
-	for x in things[ingredient]:
+for dish in mcover:
+	for x in things[dish]:
 		test[x] = True
 
 print(", ".join(mcover))
