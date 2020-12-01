901a4f783db935f95edd7e196e959bc987f6cdee
void4 <arndtschnabel@freenet.de>
2020-12-01 02:31:11 +0100

    Use LENGTH of difference set

--- greedy.py
+++ greedy.py
@@ -23,15 +23,16 @@ things = {}
 for ci, column in enumerate(columns):
 	things[dishes[ci]] = {i for i,c in enumerate(column) if c!=""}
 
-print(things)
+#print(things)
 
 mcover = set()
 cover = set()
 while len(cover) != len(ingredients):
-	mx = max(things.items(), key=lambda thing:thing[1]-cover)
+	mx = max(things.items(), key=lambda thing:len(thing[1]-cover))
 	cover = cover.union(mx[1])
 	mcover.add(mx[0])
 	print(len(cover), len(mcover))
+	print(mx)
 	print(cover, mcover)
 
 test = [False for i in range(len(ingredients))]
